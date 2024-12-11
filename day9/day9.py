7f0ea180b6a591784953f89c2cc93ae859ff9c79
trevorgarr <garrood.trevor@gmail.com>
2024-12-11 11:41:53 -0800

    update day X and initial day 9 (uncomplete)

---
+++ day9/day9.py
@@ -0,0 +1,26 @@
+"""
+2024 AOC Day 9:
+"""
+
+
+def parse_input(input):
+    return input
+
+
+def part_one(parsed_input):
+    return -1
+
+
+def part_two(parsed_input):
+    return -1
+
+
+if __name__ == "__main__":
+    f = open("test.txt", "r")
+    data = f.read()
+    input = data.splitlines()
+    parsed_input = parse_input(input)
+    print("PARSED_INPUT: ", parsed_input)
+    print("PART ONE =", part_one(parsed_input))
+    print("PART TWO =", part_two(parsed_input))
+    f.close()
